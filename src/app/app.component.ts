854f82396512e2908cbe632dc6c45ffb14240747
Peter Gál <pergalsk@gmail.com>
2021-03-04 18:38:59 +0100

    Fixed API links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -177,17 +177,17 @@ export class AppComponent implements OnInit {
   }
 
   private getSysDateTime(): Observable<any> {
-    // return this.httpClient.get('/api/sysdate');
-    return this.httpClient.get('http://localhost:4004/retaz-patmos-be/public/api/sysdate');
+    return this.httpClient.get('/api/sysdate');
+    // return this.httpClient.get('http://localhost:4004/retaz-patmos-be/public/api/sysdate');
   }
 
   private loadDates(): Observable<any> {
-    // return this.httpClient.get('/api/dates');
-    return this.httpClient.get('http://localhost:4004/retaz-patmos-be/public/api/dates');
+    return this.httpClient.get('/api/dates');
+    // return this.httpClient.get('http://localhost:4004/retaz-patmos-be/public/api/dates');
   }
 
   private submitAnswers(submitData): Observable<any> {
-    // return this.httpClient.put('/api/dates', submitData);
-    return this.httpClient.put('http://localhost:4004/retaz-patmos-be/public/api/dates?XDEBUG_SESSION_START=PHPSTORM', submitData);
+    return this.httpClient.put('/api/dates', submitData);
+    // return this.httpClient.put('http://localhost:4004/retaz-patmos-be/public/api/dates?XDEBUG_SESSION_START=PHPSTORM', submitData);
   }
 }
