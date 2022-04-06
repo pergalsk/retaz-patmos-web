162606069e68117864bd21ff372d231fbe89d8d9
Peter Gál <pergalsk@gmail.com>
2022-04-06 23:19:34 +0200

    Refactor: removed unnecessary code

--- src/app/modules/year2022/contents/day-content-panel/day-content-panel.component.ts
+++ src/app/modules/year2022/contents/day-content-panel/day-content-panel.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-day-content-panel',
   templateUrl: './day-content-panel.component.html',
   styleUrls: ['./day-content-panel.component.scss'],
 })
-export class DayContentPanelComponent implements OnInit {
+export class DayContentPanelComponent {
   @Input() date: string;
 
   firstTitle: string = 'Biblický text na zamyslenie';
   secondTitle: string = 'Modlitebné podnety na dnešný deň';
-  badgeText: string;
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
