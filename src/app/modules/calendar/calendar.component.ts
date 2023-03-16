f680865daa28d493ac63e420d6deadf575cd880e
Peter Gál <pergalsk@gmail.com>
2023-03-16 23:05:52 +0100

    Dev: removed unused styles file

--- src/app/modules/calendar/calendar.component.ts
+++ src/app/modules/calendar/calendar.component.ts
@@ -42,7 +42,6 @@ import {
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarComponent implements OnInit, OnChanges {
