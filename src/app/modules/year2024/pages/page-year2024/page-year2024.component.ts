8a6e9cbdb9d35ff90a4c07c4358a2660e5535026
Peter Gál <pergalsk@gmail.com>
2024-02-10 12:51:09 +0100

    Change: small style fixes; added new styles for verses; refactored styling for year 2024 (moved to year 2024 module)

--- src/app/modules/year2024/pages/page-year2024/page-year2024.component.ts
+++ src/app/modules/year2024/pages/page-year2024/page-year2024.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-page-year2024',
+  styleUrls: ['page-year2024.component.scss'],
   template: `
-    <app-main-content2024></app-main-content2024>
-    <app-calendar-signup [year]="year" [overrides]="overrides" />
+    <div class="theme2024">
+      <app-main-content2024></app-main-content2024>
+      <app-calendar-signup [year]="year" [overrides]="overrides" />
+    </div>
   `,
+  encapsulation: ViewEncapsulation.None,
 })
 export class PageYear2024Component {
   year: string = '2024';
