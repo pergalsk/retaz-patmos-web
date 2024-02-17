93cd5532490f6976825e44c21a6b513e299e8b7a
Peter Gál <pergalsk@gmail.com>
2024-02-17 20:45:17 +0100

    Dev: quotes component changed to input signals

--- src/app/modules/shared/components/quotes/quotes.component.ts
+++ src/app/modules/shared/components/quotes/quotes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, ViewEncapsulation } from '@angular/core';
 import { NgbCarousel, NgbSlide } from '@ng-bootstrap/ng-bootstrap';
 
 export type Quote = {
@@ -10,20 +10,19 @@ export type Quote = {
   selector: 'app-quotes',
   standalone: true,
   imports: [NgbCarousel, NgbSlide],
-  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./quotes.scss'],
   template: `
     <ngb-carousel
       class="carousel-fade"
-      [interval]="interval"
+      [interval]="interval()"
       [keyboard]="false"
       [pauseOnFocus]="false"
       [pauseOnHover]="false"
-      [showNavigationArrows]="arrows"
-      [showNavigationIndicators]="indicators"
-      [class.with-indicators]="indicators"
+      [showNavigationArrows]="arrows()"
+      [showNavigationIndicators]="indicators()"
+      [class.with-indicators]="indicators()"
     >
-      @for (quote of quotes; track $index) {
+      @for (quote of quotes(); track $index) {
         <ng-template ngbSlide>
           <figure class="slider-quotes">
             <blockquote class="blockquote">"{{ quote.text }}"</blockquote>
@@ -33,14 +32,12 @@ export type Quote = {
       }
     </ngb-carousel>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class QuotesComponent {
-  @Input({ required: true }) quotes: Quote[] = [];
-  // quotes: Signal<Quote[]> = input.required();
-
-  @Input() interval: number = 5000;
-
-  @Input() arrows: boolean = false;
-
-  @Input() indicators: boolean = false;
+  quotes = input.required<Quote[]>();
+  interval = input<number>(5000);
+  arrows = input<boolean>(false);
+  indicators = input<boolean>(false);
 }
