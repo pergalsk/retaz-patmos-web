fafad22297d200083a0dca1fb3092b935a4ec9dc
Peter Gál <pergalsk@gmail.com>
2024-02-18 22:28:31 +0100

    Dev: refactored two-split component

--- src/app/modules/shared/components/two-split/two-split.component.ts
+++ src/app/modules/shared/components/two-split/two-split.component.ts
@@ -1,15 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-two-split',
-  templateUrl: './two-split.component.html',
-  styleUrls: ['./two-split.component.scss'],
   standalone: true,
   imports: [NgIf],
+  template: `
+    @if (firstTitle() || badgeText()) {
+      <h2 class="display-5 mb-4">
+        @if (firstTitle()) {
+          <span class="title-with-badge">{{ firstTitle() }}</span>
+        }
+        @if (badgeText) {
+          <span class="date-badge">{{ badgeText() }}</span>
+        }
+      </h2>
+    }
+
+    <ng-content select="[first-slot]" />
+    @if (secondTitle()) {
+      <h2 class="display-5 mb-4 mt-4">{{ secondTitle() }}</h2>
+    }
+    <ng-content select="[second-slot]" />
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TwoSplitComponent {
-  @Input() firstTitle: any;
-  @Input() secondTitle: any;
-  @Input() badgeText: any;
+  firstTitle = input<string>();
+  secondTitle = input<string>();
+  badgeText = input<string>();
 }
